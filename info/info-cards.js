4d1013de56bb249f6cdaff12082741c304c9037d
Sinny-Hong <sinny0725@gmail.com>
2026-07-31 01:23:19 +0800

    342

---
+++ info/info-cards.js
@@ -0,0 +1,13 @@
+window.INFO_CATEGORIES = ["升學與就業"];
+
+window.INFO_DATA = [
+  {
+    id: "vocational-program",
+    category: "升學與就業",
+    tag: "綜合職能科",
+    title: "認識綜合職能科",
+    desc: "了解綜合職能科的學習內容、課程方向與未來發展。",
+    filename: "vocational-program.html",
+    themeColor: "#f59e0b"
+  }
+];
